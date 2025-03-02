ed3879f0890378c57165b1a38af0de715f48063f
Xiang Li <idaq521@gmail.com>
2025-03-02 18:05:52 +0800

    Update module_dnsacookiev.c
    Update get_name_helper_acookiev
    
    Co-Authored-By: Zixuan Xie <151416067+limerencece@users.noreply.github.com>

--- src/probe_modules/module_dnsacookiev.c
+++ src/probe_modules/module_dnsacookiev.c
@@ -345,3 +345,67 @@ static int build_global_dns_packets_acookiev(char **domains, int num_domains) {
                         name++;
                         name_len--;
                     }
+
+                                        uint16_t rec_bytes_consumed = get_name_helper_acookiev(
+                                            payload + offset, payload_len - offset, payload, payload_len,
+                                            name, name_len, recursion_level + 1);
+                                        // We are done so don't bother to increment the
+                                        // pointers.
+                                        if (rec_bytes_consumed == 0) {
+                                            log_trace("dnsacookiev",
+                                                      "_get_name_helper OUT. rec level %d failed",
+                                                      recursion_level);
+                                            return 0;
+                                        } else {
+                                            bytes_consumed += 2;
+                                            log_trace("dnsacookiev",
+                                                      "_get_name_helper OUT. rec level %d success. %d rec "
+                                                      "bytes consumed. %d bytes consumed.",
+                                                      recursion_level, rec_bytes_consumed, bytes_consumed);
+                                            return bytes_consumed;
+                                        }
+                                    } else if (byte == '\0') {
+                                        // don't bother with pointer incrementation. We're done.
+                                        bytes_consumed += 1;
+                                        log_trace("dnsacookiev",
+                                                  "_get_name_helper OUT. rec level %d success. %d bytes "
+                                                  "consumed.",
+                                                  recursion_level, bytes_consumed);
+                                        return bytes_consumed;
+                                    } else {
+                                        log_trace("dnsacookiev",
+                                                  "_get_name_helper, segment 0x%hx encountered", byte);
+                                        // We've now consumed a byte.
+                                        ++data;
+                                        --data_len;
+                                        // Mark byte consumed after we check for first
+                                        // iteration. Do we have enough data left (must have
+                                        // null byte too)?
+                                        if ((byte + 1) > data_len) {
+                                            log_trace("dnsacookiev",
+                                                      "_get_name_helper OUT. ERR. Not enough data "
+                                                      "for segment %hd");
+                                            return 0;
+                                        }
+                                        // If we've consumed any bytes and are in a label, we're
+                                        // in a label chain. We need to add a dot.
+                                        if (bytes_consumed > 0) {
+
+                                            if (name_len < 1) {
+                                                log_warn("dnsacookiev",
+                                                         "Exceeded static name field allocation.");
+                                                return 0;
+                                            }
+
+                                            name[0] = '.';
+                                            name++;
+                                            name_len--;
+                                        }
+                                        // Now we've consumed a byte.
+                                        ++bytes_consumed;
+                                        // Did we run out of our arbitrary buffer?
+                                        if (byte > name_len) {
+                                            log_warn("dnsacookiev",
+                                                     "Exceeded static name field allocation.");
+                                            return 0;
+                                        }
