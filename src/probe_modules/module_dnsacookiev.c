5b39bce081d600fcf0358aee2e0c2bb05468c70c
Xiang Li <idaq521@gmail.com>
2025-03-02 18:11:40 +0800

    Update module_dnsacookiev.c
    Update process_response_answer_acookiev
    
    Co-Authored-By: Zixuan Xie <151416067+limerencece@users.noreply.github.com>

--- src/probe_modules/module_dnsacookiev.c
+++ src/probe_modules/module_dnsacookiev.c
@@ -561,3 +561,83 @@ static bool process_response_answer_acookiev(char **data, uint16_t *data_len,
         fs_add_uint64(afs, "ttl", ttl);
         fs_add_uint64(afs, "rdlength", rdlength);
     }
+
+        // XXX Fill this out for the other types we care about.
+        if (type == DNS_QTYPE_NS || type == DNS_QTYPE_CNAME) {
+            uint16_t rdata_bytes_consumed = 0;
+            char    *rdata_name           = get_name_acookiev(
+                rdata, rdlength, payload, payload_len, &rdata_bytes_consumed);
+            if (rdata_name == NULL) {
+                fs_add_uint64(afs, "rdata_is_parsed", 0);
+                fs_add_binary(afs, "rdata", rdlength, rdata, 0);
+            } else {
+                fs_add_uint64(afs, "rdata_is_parsed", 1);
+                fs_add_unsafe_string(afs, "rdata", rdata_name, 1);
+            }
+        } else if (type == DNS_QTYPE_MX) {
+            uint16_t rdata_bytes_consumed = 0;
+            if (rdlength <= 4) {
+                fs_add_uint64(afs, "rdata_is_parsed", 0);
+                fs_add_binary(afs, "rdata", rdlength, rdata, 0);
+            } else {
+                char *rdata_name =
+                    get_name_acookiev(rdata + 2, rdlength - 2, payload, payload_len,
+                                      &rdata_bytes_consumed);
+                if (rdata_name == NULL) {
+                    fs_add_uint64(afs, "rdata_is_parsed", 0);
+                    fs_add_binary(afs, "rdata", rdlength, rdata, 0);
+                } else {
+                    // (largest value 16bit) + " " + answer + null
+                    char *rdata_with_pref = xmalloc(5 + 1 + strlen(rdata_name) + 1);
+
+                    uint8_t num_printed = snprintf(rdata_with_pref, 6, "%hu ",
+                                                   ntohs(*(uint16_t *) rdata));
+                    memcpy(rdata_with_pref + num_printed, rdata_name,
+                           strlen(rdata_name));
+                    fs_add_uint64(afs, "rdata_is_parsed", 1);
+                    fs_add_unsafe_string(afs, "rdata", rdata_with_pref, 1);
+                }
+            }
+        } else if (type == DNS_QTYPE_TXT) {
+            if (rdlength >= 1 && (rdlength - 1) != *(uint8_t *) rdata) {
+                log_warn("dnsacookiev",
+                         "TXT record with wrong TXT len. Not processing.");
+                fs_add_uint64(afs, "rdata_is_parsed", 0);
+                fs_add_binary(afs, "rdata", rdlength, rdata, 0);
+            } else if (rdlength < 1) {
+                fs_add_uint64(afs, "rdata_is_parsed", 0);
+                fs_add_binary(afs, "rdata", rdlength, rdata, 0);
+            } else {
+                fs_add_uint64(afs, "rdata_is_parsed", 1);
+                char *txt = xmalloc(rdlength);
+                memcpy(txt, rdata + 1, rdlength - 1);
+                fs_add_unsafe_string(afs, "rdata", txt, 1);
+            }
+        } else if (type == DNS_QTYPE_A) {
+            if (rdlength != 4) {
+                log_warn("dnsacookiev",
+                         "A record with IP of length %d. Not processing.",
+                         rdlength);
+                fs_add_uint64(afs, "rdata_is_parsed", 0);
+                fs_add_binary(afs, "rdata", rdlength, rdata, 0);
+            } else {
+                fs_add_uint64(afs, "rdata_is_parsed", 1);
+                char *addr = strdup(inet_ntoa(*(struct in_addr *) rdata));
+                fs_add_unsafe_string(afs, "rdata", addr, 1);
+            }
+        } else if (type == DNS_QTYPE_AAAA) {
+            if (rdlength != 16) {
+                log_warn("dnsacookiev",
+                         "AAAA record with IP of length %d. Not processing.",
+                         rdlength);
+                fs_add_uint64(afs, "rdata_is_parsed", 0);
+                fs_add_binary(afs, "rdata", rdlength, rdata, 0);
+            } else {
+                fs_add_uint64(afs, "rdata_is_parsed", 1);
+                char *ipv6_str = xmalloc(INET6_ADDRSTRLEN);
+
+                inet_ntop(AF_INET6, (struct sockaddr_in6 *) rdata, ipv6_str,
+                          INET6_ADDRSTRLEN);
+
+                fs_add_unsafe_string(afs, "rdata", ipv6_str, 1);
+            }
