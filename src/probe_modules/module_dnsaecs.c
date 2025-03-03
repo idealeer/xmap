19213d458c61de12d44032a8d26dc10515b8766a
Xiang Li <idaq521@gmail.com>
2025-03-03 13:49:26 +0800

    Update module_dnsaecs.c
    Update process_response_answer_aecs

--- src/probe_modules/module_dnsaecs.c
+++ src/probe_modules/module_dnsaecs.c
@@ -554,176 +554,176 @@ static bool process_response_answer_aecs(char **data, uint16_t *data_len,
         fs_add_uint64(afs, "rdlength", rdlength);
     }
 
-        // XXX Fill this out for the other types we care about.
-        if (type == DNS_QTYPE_NS || type == DNS_QTYPE_CNAME) {
-            uint16_t rdata_bytes_consumed = 0;
-            char *rdata_name = get_name_aecs(rdata, rdlength, payload, payload_len,
-                                                          &rdata_bytes_consumed);
+    // XXX Fill this out for the other types we care about.
+    if (type == DNS_QTYPE_NS || type == DNS_QTYPE_CNAME) {
+        uint16_t rdata_bytes_consumed = 0;
+        char *rdata_name = get_name_aecs(rdata, rdlength, payload, payload_len,
+                                         &rdata_bytes_consumed);
+        if (rdata_name == NULL) {
+            fs_add_uint64(afs, "rdata_is_parsed", 0);
+            fs_add_binary(afs, "rdata", rdlength, rdata, 0);
+        } else {
+            fs_add_uint64(afs, "rdata_is_parsed", 1);
+            fs_add_unsafe_string(afs, "rdata", rdata_name, 1);
+        }
+    } else if (type == DNS_QTYPE_MX) {
+        uint16_t rdata_bytes_consumed = 0;
+        if (rdlength <= 4) {
+            fs_add_uint64(afs, "rdata_is_parsed", 0);
+            fs_add_binary(afs, "rdata", rdlength, rdata, 0);
+        } else {
+            char *rdata_name =
+                get_name_aecs(rdata + 2, rdlength - 2, payload, payload_len,
+                              &rdata_bytes_consumed);
             if (rdata_name == NULL) {
                 fs_add_uint64(afs, "rdata_is_parsed", 0);
                 fs_add_binary(afs, "rdata", rdlength, rdata, 0);
             } else {
-                fs_add_uint64(afs, "rdata_is_parsed", 1);
-                fs_add_unsafe_string(afs, "rdata", rdata_name, 1);
-            }
-        } else if (type == DNS_QTYPE_MX) {
-            uint16_t rdata_bytes_consumed = 0;
-            if (rdlength <= 4) {
-                fs_add_uint64(afs, "rdata_is_parsed", 0);
-                fs_add_binary(afs, "rdata", rdlength, rdata, 0);
-            } else {
-                char *rdata_name =
-                    get_name_aecs(rdata + 2, rdlength - 2, payload, payload_len,
-                                  &rdata_bytes_consumed);
-                if (rdata_name == NULL) {
-                    fs_add_uint64(afs, "rdata_is_parsed", 0);
-                    fs_add_binary(afs, "rdata", rdlength, rdata, 0);
-                } else {
-                    // (largest value 16bit) + " " + answer + null
-                    char *rdata_with_pref = xmalloc(5 + 1 + strlen(rdata_name) + 1);
+                // (largest value 16bit) + " " + answer + null
+                char *rdata_with_pref = xmalloc(5 + 1 + strlen(rdata_name) + 1);
 
-                    uint8_t num_printed = snprintf(rdata_with_pref, 6, "%hu ",
-                                                   ntohs(*(uint16_t *) rdata));
-                    memcpy(rdata_with_pref + num_printed, rdata_name,
-                           strlen(rdata_name));
-                    fs_add_uint64(afs, "rdata_is_parsed", 1);
-                    fs_add_unsafe_string(afs, "rdata", rdata_with_pref, 1);
+                uint8_t num_printed = snprintf(rdata_with_pref, 6, "%hu ",
+                                               ntohs(*(uint16_t *) rdata));
+                memcpy(rdata_with_pref + num_printed, rdata_name,
+                       strlen(rdata_name));
+                fs_add_uint64(afs, "rdata_is_parsed", 1);
+                fs_add_unsafe_string(afs, "rdata", rdata_with_pref, 1);
+            }
+        }
+    } else if (type == DNS_QTYPE_TXT) {
+        if (rdlength >= 1 && (rdlength - 1) != *(uint8_t *) rdata) {
+            log_warn("dnsaecs",
+                     "TXT record with wrong TXT len. Not processing.");
+            fs_add_uint64(afs, "rdata_is_parsed", 0);
+            fs_add_binary(afs, "rdata", rdlength, rdata, 0);
+        } else if (rdlength < 1) {
+            fs_add_uint64(afs, "rdata_is_parsed", 0);
+            fs_add_binary(afs, "rdata", rdlength, rdata, 0);
+        } else {
+            fs_add_uint64(afs, "rdata_is_parsed", 1);
+            char *txt = xmalloc(rdlength);
+            memcpy(txt, rdata + 1, rdlength - 1);
+            fs_add_unsafe_string(afs, "rdata", txt, 1);
+        }
+    } else if (type == DNS_QTYPE_A) {
+        if (rdlength != 4) {
+            log_warn("dnsaecs",
+                     "A record with IP of length %d. Not processing.",
+                     rdlength);
+            fs_add_uint64(afs, "rdata_is_parsed", 0);
+            fs_add_binary(afs, "rdata", rdlength, rdata, 0);
+        } else {
+            fs_add_uint64(afs, "rdata_is_parsed", 1);
+            char *addr = strdup(inet_ntoa(*(struct in_addr *) rdata));
+            fs_add_unsafe_string(afs, "rdata", addr, 1);
+        }
+    } else if (type == DNS_QTYPE_AAAA) {
+        if (rdlength != 16) {
+            log_warn("dnsaecs",
+                     "AAAA record with IP of length %d. Not processing.",
+                     rdlength);
+            fs_add_uint64(afs, "rdata_is_parsed", 0);
+            fs_add_binary(afs, "rdata", rdlength, rdata, 0);
+        } else {
+            fs_add_uint64(afs, "rdata_is_parsed", 1);
+            char *ipv6_str = xmalloc(INET6_ADDRSTRLEN);
+
+            inet_ntop(AF_INET6, (struct sockaddr_in6 *) rdata, ipv6_str,
+                      INET6_ADDRSTRLEN);
+
+            fs_add_unsafe_string(afs, "rdata", ipv6_str, 1);
+        }
+    } else if (type == DNS_QTYPE_SIG || type == DNS_QTYPE_SRV ||
+               type == DNS_QTYPE_DS || type == DNS_QTYPE_DNSKEY ||
+               type == DNS_QTYPE_TLSA || type == DNS_QTYPE_SVCB ||
+               type == DNS_QTYPE_HTTPS || type == DNS_QTYPE_CAA ||
+               type == DNS_QTYPE_HTTPSSVC) {
+        if (rdlength >= 1 && (rdlength - 1) != *(uint8_t *) rdata) {
+            log_warn(
+                "dnsaecs",
+                "SRV-like record with wrong SRV-like len. Not processing.");
+            fs_add_uint64(afs, "rdata_is_parsed", 0);
+            fs_add_binary(afs, "rdata", rdlength, rdata, 0);
+        } else if (rdlength < 1) {
+            fs_add_uint64(afs, "rdata_is_parsed", 0);
+            fs_add_binary(afs, "rdata", rdlength, rdata, 0);
+        } else {
+            fs_add_uint64(afs, "rdata_is_parsed", 1);
+            char *txt = xmalloc(rdlength);
+            memcpy(txt, rdata + 1, rdlength - 1);
+            fs_add_unsafe_string(afs, "rdata", txt, 1);
+        }
+    } else if (type == DNS_QTYPE_OPT) {
+        dns_option_tail *option_tail =
+            (dns_option_tail *) (*data + bytes_consumed);
+        uint16_t udpsize  = ntohs(option_tail->udpsize);
+        uint8_t  ercode   = option_tail->ercode;
+        uint8_t  eversion = option_tail->eversion;
+        uint16_t dodnssec = option_tail->dodnssec;
+        uint16_t option_z =
+            (((option_tail->dodnssec << 7) + option_tail->z1) << 8) +
+            option_tail->z2;
+        uint16_t option_dlength = ntohs(option_tail->dlength);
+        char    *option_data    = option_tail->data;
+
+        fs_add_uint64(afs, "udpsize", udpsize);
+        fs_add_uint64(afs, "ercode", ercode);
+        fs_add_uint64(afs, "eversion", eversion);
+        fs_add_uint64(afs, "dodnssec", dodnssec);
+        fs_add_uint64(afs, "z", option_z);
+        fs_add_uint64(afs, "dlength", option_dlength);
+        fs_add_binary(afs, "data", option_dlength, option_data, 0);
+
+        if (option_dlength >= 4) {
+            dns_option_ecs *ecs_tail = (dns_option_ecs *) option_data;
+            uint16_t        optcode  = ntohs(ecs_tail->optcode);
+
+            fs_add_uint64(afs, "optcode", optcode);
+
+            if (optcode == DNS_OPTCODE_ECS) {
+                fs_add_string(afs, "optcode_str", "ECS", 0);
+
+                uint16_t optlength = ntohs(ecs_tail->optlength);
+                uint16_t family    = ntohs(ecs_tail->family);
+                uint8_t  srcnmask  = ecs_tail->srcnmask;
+                uint8_t  scpnmask  = ecs_tail->scpnmask;
+
+                fs_add_uint64(afs, "optlength", optlength);
+                fs_add_uint64(afs, "family", family);
+                fs_add_uint64(afs, "srcnmask", srcnmask);
+                fs_add_uint64(afs, "scpnmask", scpnmask);
+
+                if (family == DNS_ADDRFAMILY_IP) {
+                    uint8_t ip_raw[4] = {00};
+                    memcpy(ip_raw, ecs_tail->cs, optlength - 4);
+
+                    fs_add_string(afs, "cs",
+                                  make_ip_str((uint32_t) ip_raw[3] << 24 |
+                                              (uint32_t) ip_raw[2] << 16 |
+                                              (uint32_t) ip_raw[1] << 8 |
+                                              (uint32_t) ip_raw[0]),
+                                  1);
+                } else if (family == DNS_ADDRFAMILY_IP6) {
+                    uint8_t ip_raw[16] = {0x00};
+                    memcpy(ip_raw, ecs_tail->cs, optlength - 4);
+
+                    fs_add_string(afs, "cs",
+                                  make_ipv6_str((struct in6_addr *) ip_raw), 1);
                 }
             }
-        } else if (type == DNS_QTYPE_TXT) {
-            if (rdlength >= 1 && (rdlength - 1) != *(uint8_t *) rdata) {
-                log_warn("dnsaecs",
-                         "TXT record with wrong TXT len. Not processing.");
-                fs_add_uint64(afs, "rdata_is_parsed", 0);
-                fs_add_binary(afs, "rdata", rdlength, rdata, 0);
-            } else if (rdlength < 1) {
-                fs_add_uint64(afs, "rdata_is_parsed", 0);
-                fs_add_binary(afs, "rdata", rdlength, rdata, 0);
-            } else {
-                fs_add_uint64(afs, "rdata_is_parsed", 1);
-                char *txt = xmalloc(rdlength);
-                memcpy(txt, rdata + 1, rdlength - 1);
-                fs_add_unsafe_string(afs, "rdata", txt, 1);
-            }
-        } else if (type == DNS_QTYPE_A) {
-            if (rdlength != 4) {
-                log_warn("dnsaecs",
-                         "A record with IP of length %d. Not processing.",
-                         rdlength);
-                fs_add_uint64(afs, "rdata_is_parsed", 0);
-                fs_add_binary(afs, "rdata", rdlength, rdata, 0);
-            } else {
-                fs_add_uint64(afs, "rdata_is_parsed", 1);
-                char *addr = strdup(inet_ntoa(*(struct in_addr *) rdata));
-                fs_add_unsafe_string(afs, "rdata", addr, 1);
-            }
-        } else if (type == DNS_QTYPE_AAAA) {
-            if (rdlength != 16) {
-                log_warn("dnsaecs",
-                         "AAAA record with IP of length %d. Not processing.",
-                         rdlength);
-                fs_add_uint64(afs, "rdata_is_parsed", 0);
-                fs_add_binary(afs, "rdata", rdlength, rdata, 0);
-            } else {
-                fs_add_uint64(afs, "rdata_is_parsed", 1);
-                char *ipv6_str = xmalloc(INET6_ADDRSTRLEN);
+        }
+    } else {
+        fs_add_uint64(afs, "rdata_is_parsed", 0);
+        fs_add_binary(afs, "rdata", rdlength, rdata, 0);
+    }
+    // Now we're adding the new fs to the list.
+    fs_add_fieldset(list, NULL, afs);
+    // Now update the pointers.
+    *data     = *data + bytes_consumed + sizeof(dns_answer_tail) + rdlength;
+    *data_len = *data_len - bytes_consumed - sizeof(dns_answer_tail) - rdlength;
+    log_trace("dnsaecs",
+              "return success from process_response_answer_aecs, data_len: %d",
+              *data_len);
 
-                inet_ntop(AF_INET6, (struct sockaddr_in6 *) rdata, ipv6_str,
-                          INET6_ADDRSTRLEN);
-
-                fs_add_unsafe_string(afs, "rdata", ipv6_str, 1);
-            }
-        } else if (type == DNS_QTYPE_SIG || type == DNS_QTYPE_SRV ||
-                   type == DNS_QTYPE_DS || type == DNS_QTYPE_DNSKEY ||
-                   type == DNS_QTYPE_TLSA || type == DNS_QTYPE_SVCB ||
-                   type == DNS_QTYPE_HTTPS || type == DNS_QTYPE_CAA ||
-                   type == DNS_QTYPE_HTTPSSVC) {
-            if (rdlength >= 1 && (rdlength - 1) != *(uint8_t *) rdata) {
-                log_warn(
-                    "dnsaecs",
-                    "SRV-like record with wrong SRV-like len. Not processing.");
-                fs_add_uint64(afs, "rdata_is_parsed", 0);
-                fs_add_binary(afs, "rdata", rdlength, rdata, 0);
-            } else if (rdlength < 1) {
-                fs_add_uint64(afs, "rdata_is_parsed", 0);
-                fs_add_binary(afs, "rdata", rdlength, rdata, 0);
-            } else {
-                fs_add_uint64(afs, "rdata_is_parsed", 1);
-                char *txt = xmalloc(rdlength);
-                memcpy(txt, rdata + 1, rdlength - 1);
-                fs_add_unsafe_string(afs, "rdata", txt, 1);
-            }
-        } else if (type == DNS_QTYPE_OPT) {
-            dns_option_tail *option_tail =
-                (dns_option_tail *) (*data + bytes_consumed);
-            uint16_t udpsize  = ntohs(option_tail->udpsize);
-            uint8_t  ercode   = option_tail->ercode;
-            uint8_t  eversion = option_tail->eversion;
-            uint16_t dodnssec = option_tail->dodnssec;
-            uint16_t option_z =
-                (((option_tail->dodnssec << 7) + option_tail->z1) << 8) +
-                option_tail->z2;
-            uint16_t option_dlength = ntohs(option_tail->dlength);
-            char    *option_data    = option_tail->data;
-
-            fs_add_uint64(afs, "udpsize", udpsize);
-            fs_add_uint64(afs, "ercode", ercode);
-            fs_add_uint64(afs, "eversion", eversion);
-            fs_add_uint64(afs, "dodnssec", dodnssec);
-            fs_add_uint64(afs, "z", option_z);
-            fs_add_uint64(afs, "dlength", option_dlength);
-            fs_add_binary(afs, "data", option_dlength, option_data, 0);
-
-                        if (option_dlength >= 4) {
-                            dns_option_ecs *ecs_tail = (dns_option_ecs *) option_data;
-                            uint16_t        optcode  = ntohs(ecs_tail->optcode);
-
-                            fs_add_uint64(afs, "optcode", optcode);
-
-                            if (optcode == DNS_OPTCODE_ECS) {
-                                fs_add_string(afs, "optcode_str", "ECS", 0);
-
-                                uint16_t optlength = ntohs(ecs_tail->optlength);
-                                uint16_t family    = ntohs(ecs_tail->family);
-                                uint8_t  srcnmask  = ecs_tail->srcnmask;
-                                uint8_t  scpnmask  = ecs_tail->scpnmask;
-
-                                fs_add_uint64(afs, "optlength", optlength);
-                                fs_add_uint64(afs, "family", family);
-                                fs_add_uint64(afs, "srcnmask", srcnmask);
-                                fs_add_uint64(afs, "scpnmask", scpnmask);
-
-                                if (family == DNS_ADDRFAMILY_IP) {
-                                    uint8_t ip_raw[4] = {00};
-                                    memcpy(ip_raw, ecs_tail->cs, optlength - 4);
-
-                                    fs_add_string(afs, "cs",
-                                                  make_ip_str((uint32_t) ip_raw[3] << 24 |
-                                                              (uint32_t) ip_raw[2] << 16 |
-                                                              (uint32_t) ip_raw[1] << 8 |
-                                                              (uint32_t) ip_raw[0]),
-                                                  1);
-                                } else if (family == DNS_ADDRFAMILY_IP6) {
-                                    uint8_t ip_raw[16] = {0x00};
-                                    memcpy(ip_raw, ecs_tail->cs, optlength - 4);
-
-                                    fs_add_string(afs, "cs",
-                                                  make_ipv6_str((struct in6_addr *) ip_raw), 1);
-                                }
-                            }
-                        }
-                    } else {
-                        fs_add_uint64(afs, "rdata_is_parsed", 0);
-                        fs_add_binary(afs, "rdata", rdlength, rdata, 0);
-                    }
-                    // Now we're adding the new fs to the list.
-                    fs_add_fieldset(list, NULL, afs);
-                    // Now update the pointers.
-                    *data     = *data + bytes_consumed + sizeof(dns_answer_tail) + rdlength;
-                    *data_len = *data_len - bytes_consumed - sizeof(dns_answer_tail) - rdlength;
-                    log_trace("dnsaecs",
-                              "return success from process_response_answer_aecs, data_len: %d",
-                              *data_len);
-
-                    return 0;
-            }
+    return 0;
+}
