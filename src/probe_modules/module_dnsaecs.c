b42ccac87c4baf683a56dc9da5f215e53889c514
Xiang Li <idaq521@gmail.com>
2025-03-03 14:23:05 +0800

    Update module_dnsaecs.c
    Add dnsaecs_make_packet

--- src/probe_modules/module_dnsaecs.c
+++ src/probe_modules/module_dnsaecs.c
@@ -993,3 +993,71 @@ static int dnsaecs_global_cleanup(UNUSED struct state_conf *xconf,
 
     return EXIT_SUCCESS;
 }
+
+int dnsaecs_thread_init(void *buf, macaddr_t *src, macaddr_t *gw,
+                        void **arg_ptr) {
+    memset(buf, 0, MAX_PACKET_SIZE);
+
+    // Setup assuming num_questions_aecs == 0
+    struct ether_header *eth_header = (struct ether_header *) buf;
+    make_eth_header(eth_header, src, gw);
+
+    struct ip *ip_header = (struct ip *) (&eth_header[1]);
+    uint16_t   ip_len =
+        sizeof(struct ip) + sizeof(struct udphdr) + dns_packet_lens_aecs[0];
+    make_ip_header(ip_header, IPPROTO_UDP, ip_len);
+
+    struct udphdr *udp_header = (struct udphdr *) (&ip_header[1]);
+    uint16_t       udp_len    = sizeof(struct udphdr) + dns_packet_lens_aecs[0];
+    make_udp_header(udp_header, udp_len);
+
+    char *payload                = (char *) (&udp_header[1]);
+    module_dnsaecs.packet_length = sizeof(struct ether_header) +
+                                   sizeof(struct ip) + sizeof(struct udphdr) +
+                                   dns_packet_lens_aecs[0];
+    assert(module_dnsaecs.packet_length <= MAX_PACKET_SIZE);
+
+    memcpy(payload, dns_packets_aecs[0], dns_packet_lens_aecs[0]);
+
+    // Seed our random number generator with the global generator
+    uint32_t   seed = aesrand_getword(xconf.aes);
+    aesrand_t *aes  = aesrand_init_from_seed(seed);
+    *arg_ptr        = aes;
+
+    return EXIT_SUCCESS;
+}
+
+int dnsaecs_make_packet(void *buf, size_t *buf_len, ipaddr_n_t *src_ip,
+                        ipaddr_n_t *dst_ip, port_h_t dst_port, uint8_t ttl,
+                        int probe_num, index_h_t index, void *arg) {
+    struct ether_header *eth_header = (struct ether_header *) buf;
+    struct ip           *ip_header  = (struct ip *) (&eth_header[1]);
+    struct udphdr       *udp_header = (struct udphdr *) (&ip_header[1]);
+
+    uint8_t validation[VALIDATE_BYTES];
+    validate_gen(src_ip, dst_ip, dst_port, validation);
+
+    port_h_t src_port = get_src_port(dns_num_ports_aecs, probe_num, validation);
+    uint16_t dns_txid = get_dnsa_txid(validation, probe_num);
+
+    if (label_type_aecs == DNS_LTYPE_RAW || label_type_aecs == DNS_LTYPE_STR) {
+        // For num_questions_aecs == 1, we handle this in per-thread init. Do
+        // less work
+        if (num_questions_aecs > 1) {
+            uint16_t ip_len = sizeof(struct ip) + sizeof(struct udphdr) +
+                              dns_packet_lens_aecs[index];
+            make_ip_header(ip_header, IPPROTO_UDP, ip_len);
+
+            uint16_t udp_len =
+                sizeof(struct udphdr) + dns_packet_lens_aecs[index];
+            make_udp_header(udp_header, udp_len);
+
+            char *payload = (char *) (&udp_header[1]);
+            *buf_len      = sizeof(struct ether_header) + sizeof(struct ip) +
+                       sizeof(struct udphdr) + dns_packet_lens_aecs[index];
+
+            assert(*buf_len <= MAX_PACKET_SIZE);
+
+            memcpy(payload, dns_packets_aecs[index],
+                   dns_packet_lens_aecs[index]);
+        }
