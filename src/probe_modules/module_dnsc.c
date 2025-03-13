ad0cd41dbaf04df5eaab2079de49a2410ffa76f8
Xiang Li <idaq521@gmail.com>
2025-03-13 13:31:29 +0800

    Update module_dnsc.c
    Update process_response_answer_c
    
    Co-Authored-By: DrNbnonono <146108936+DrNbnonono@users.noreply.github.com>

--- src/probe_modules/module_dnsc.c
+++ src/probe_modules/module_dnsc.c
@@ -510,4 +510,68 @@ static bool process_response_answer_c(char **data, uint16_t *data_len,
             fs_add_uint64(afs, "rdata_is_parsed", 1);
             fs_add_unsafe_string(afs, "rdata", rdata_name, 1);
         }
+    } else if (type == DNS_QTYPE_MX) {
+        uint16_t rdata_bytes_consumed = 0;
+        if (rdlength <= 4) {
+            fs_add_uint64(afs, "rdata_is_parsed", 0);
+            fs_add_binary(afs, "rdata", rdlength, rdata, 0);
+        } else {
+            char *rdata_name = get_name_c(rdata + 2, rdlength - 2, payload,
+                                          payload_len, &rdata_bytes_consumed);
+            if (rdata_name == NULL) {
+                fs_add_uint64(afs, "rdata_is_parsed", 0);
+                fs_add_binary(afs, "rdata", rdlength, rdata, 0);
+            } else {
+                // (largest value 16bit) + " " + answer + null
+                char *rdata_with_pref = xmalloc(5 + 1 + strlen(rdata_name) + 1);
+
+                uint8_t num_printed = snprintf(rdata_with_pref, 6, "%hu ",
+                                               ntohs(*(uint16_t *) rdata));
+                memcpy(rdata_with_pref + num_printed, rdata_name,
+                       strlen(rdata_name));
+                fs_add_uint64(afs, "rdata_is_parsed", 1);
+                fs_add_unsafe_string(afs, "rdata", rdata_with_pref, 1);
+            }
+        }
+    } else if (type == DNS_QTYPE_TXT) {
+        if (rdlength >= 1 && (rdlength - 1) != *(uint8_t *) rdata) {
+            log_warn("dnsc", "TXT record with wrong TXT len. Not processing.");
+            fs_add_uint64(afs, "rdata_is_parsed", 0);
+            fs_add_binary(afs, "rdata", rdlength, rdata, 0);
+        } else if (rdlength < 1) {
+            fs_add_uint64(afs, "rdata_is_parsed", 0);
+            fs_add_binary(afs, "rdata", rdlength, rdata, 0);
+        } else {
+            fs_add_uint64(afs, "rdata_is_parsed", 1);
+            char *txt = xmalloc(rdlength);
+            memcpy(txt, rdata + 1, rdlength - 1);
+            fs_add_unsafe_string(afs, "rdata", txt, 1);
+        }
+    } else if (type == DNS_QTYPE_A) {
+        if (rdlength != 4) {
+            log_warn("dnsc", "A record with IP of length %d. Not processing.",
+                     rdlength);
+            fs_add_uint64(afs, "rdata_is_parsed", 0);
+            fs_add_binary(afs, "rdata", rdlength, rdata, 0);
+        } else {
+            fs_add_uint64(afs, "rdata_is_parsed", 1);
+            char *addr = strdup(inet_ntoa(*(struct in_addr *) rdata));
+            fs_add_unsafe_string(afs, "rdata", addr, 1);
+        }
+    } else if (type == DNS_QTYPE_AAAA) {
+        if (rdlength != 16) {
+            log_warn("dnsc",
+                     "AAAA record with IP of length %d. Not processing.",
+                     rdlength);
+            fs_add_uint64(afs, "rdata_is_parsed", 0);
+            fs_add_binary(afs, "rdata", rdlength, rdata, 0);
+        } else {
+            fs_add_uint64(afs, "rdata_is_parsed", 1);
+            char *ipv6_str = xmalloc(INET6_ADDRSTRLEN);
+
+            inet_ntop(AF_INET6, (struct sockaddr_in6 *) rdata, ipv6_str,
+                      INET6_ADDRSTRLEN);
+
+            fs_add_unsafe_string(afs, "rdata", ipv6_str, 1);
+        }
     }
